9597fcd68c2210d7074a1a2cc55c6ff53cdf0fef
renosyah <syahputrareno975@gmail.com>
2020-07-30 23:27:59 +0700

    add doc in mai js

--- main.js
+++ main.js
@@ -1,93 +1,160 @@
+
+// url dasar yang akan diakses
 const baseURL = "http://localhost:80/"
+
+// membuat object vue
 new Vue({
+
+    // tempelkan ke element html dengan id app
     el: '#app',
+
+    // membuat fungsi data
     data() {
+
+        // yang akan mengembalikan object
         return {
-            page : {
-                name : "loading-page",
-            },
+
+            // object untuk menentukan 
+            // user sedang berada di halaman mana
+            page : { name : "loading-page" },
+
+            // data array untuk kategori
             categories:[],
+
+            // data array untuk fasilitas
             facilities:[],
+
+            // data array untuk harga tiket
             ticket_prices:[],
+
+            // data array untuk jarak
             distances:[],
+
+            // data array untuk umur
             ages:[],
+
+            // parameter untuk request
+            // data pariwisata
             param : {
+
+                // id kategori yg dipilih
                 category_choosed : 0,
-                facility : {
-                    label : "Fasilitas Wisata",
-                    value : "",
-                },
-                distance : {
-                    label : "Jarak",
-                    min_value : "",
-                    max_value : ""
-                },
-                ticket_price : {
-                    label : "Tiket Masuk",
-                    min_value : "",
-                    max_value : ""
-                },
-                age : {
-                    label : "Pilih Umur",
-                    min_value : "",
-                    max_value : ""
-                }
+
+                // data request untuk fasilias
+                facility : { label : "Fasilitas Wisata", value : "" },
+
+                // data request untuk jarak
+                distance : { label : "Jarak", min_value : "", max_value : "" },
+
+                // data request untuk harga tiket
+                ticket_price : { label : "Tiket Masuk", min_value : "", max_value : "" },
+
+                // data request untuk umur
+                age : { label : "Pilih Umur", min_value : "", max_value : "" }
             },
+
+            // data array untuk menampung 
+            // respon data pariwisata
             results : [],
-            detail : {
-                id : 0,
-                kategori_id : 0,
-                nama : "",
-                lokasi : "",
-                jarak : 0,
-                deskripsi : "",
-            },
+
+            // data detail untuk menampung 
+            // respon detail data pariwisata
+            detail : { id : 0, kategori_id : 0,  nama : "", lokasi : "",  jarak : 0, deskripsi : "" },
+
+            // status untuk menunjukan
+            // apakah perangkat offline
+            // atau sedang online
             is_online : true
         }
     },
+
+    // fungsi yang akan dipanggil saat aplikasi di run
     created(){
-        window.addEventListener('offline', () => {
-            this.is_online = false
-        })
-        window.addEventListener('online', () => {
-            this.is_online = true
-        })
+
+        // fungsi yang akan menghandle kondisi saat perangkat offline
+        window.addEventListener('offline', () => { this.is_online = false })
+
+        // fungsi yang akan menghandle kondisi saat perangkat online
+        window.addEventListener('online', () => { this.is_online = true })
+
+        // memanggil fungsi untuk merequest data
+        // yang akan digunakan sebagai opsi pilihan
+        // saat user akan menginput data
         this.loadPageData()
     },
+
+    // fungsi yang akan dipanggil saat view di render
     mounted () {
+
+        // inisialisasi fungsi dropdown materialize
+        // drop down class
         window.$('.dropdown-trigger').dropdown();
     },
+
+    // kumpulan fungsi2 yang akan digunakan
+    // dalam instance ini
     methods : {
-        switchPage(name){
-            this.page.name = name
-        },
-        chooseCategory(value){
-            this.param.category_choosed = value
-            this.switchPage("criteria-page")
-        },
+
+        // yang akan dipanggil saat ingin mengganti tampilan page
+        switchPage(name){ this.page.name = name },
+
+        // yang akan dipanggil saat kategori telah dipilih
+        // dan akan dialihkan ke halaman kriteria
+        chooseCategory(value){ this.param.category_choosed = value; this.switchPage("criteria-page")  },
+        
+        // yang akan dipanggil saat ingin merequest opsi pilihan
+        // yg akan digunakan saat user akan menginput data
         loadPageData(){
 
+            // alihkan ke halaman loading
             this.switchPage("loading-page")
 
+            // menggunakan library axio
+            // untuk melakukan http request
             axios
+
+                // url target
                 .get(baseURL + 'data.json')
+
+                // saat response didapat
                 .then(response => {
                     
+                    // isi data array kategori
                     this.categories = response.data.categories;
+
+                    // isi data array fasilitas
                     this.facilities = response.data.facilities ;
+
+                    // isi data array harga tiket
                     this.ticket_prices = response.data.ticket_prices;
+
+                    // isi data array jarak
                     this.distances = response.data.distances;
+
+                    // isi data array umur
                     this.ages = response.data.ages;
+
+                    // alihkan ke halaman kategori
                     this.switchPage("category-page")
                 })
+
+                // saat error didapat
                 .catch(errors => {
 
+                    // tampilkan log
                     console.log(errors)
+
+                    // alihkan ke halaman kategori
                     this.switchPage("category-page")
                 }) 
         },
+
+        // fungsi yang akan dipanggil saat
+        // merequest data pariwisata
+        // untuk kategori non-goa
         getSAWResult(){
 
+            // alihkan ke halaman loading
             this.switchPage("loading-page")
 
             let category = "kategori_id=" + this.param.category_choosed
@@ -97,22 +164,36 @@ new Vue({
             let min_distance = "min_jarak=" + this.param.distance.min_value
             let max_distance = "max_jarak=" + this.param.distance.max_value
 
+            // menggunakan library axio
+            // untuk melakukan http request
             axios
+
+                // url target
                 .get(baseURL + 'api/all_data_pariwisata_spk.php?'+ category +'&'+ facility +'&'+ min_ticket_price +'&'+ max_ticket_price+'&'+min_distance+'&'+max_distance)
+                
+                // saat response didapat
                 .then(response => {
                     
                     this.results = response.data.data
                     this.switchPage("result-page")
                 })
+
+                // saat error didapat
                 .catch(errors => {
 
                     console.log(errors)
                     this.switchPage("result-page")
                 })  
         },
+
+        // fungsi yang akan dipanggil saat
+        // merequest data pariwisata
+        // untuk kategori goa
         getSAWResultGoa(){
 
+            // alihkan ke halaman loading
             this.switchPage("loading-page")
+            
             let category = "kategori_id=" + this.param.category_choosed
             let facility = "fasilitas_id=" + this.param.facility.value
             let min_ticket_price = "min_tiket_masuk=" + this.param.ticket_price.min_value
@@ -122,13 +203,21 @@ new Vue({
             let min_age = "min_umur=" + this.param.age.min_value
             let max_age = "max_umur=" + this.param.age.max_value
 
+            // menggunakan library axio
+            // untuk melakukan http request
             axios
+
+                // url target
                 .get(baseURL + 'api/all_data_pariwisata_spk_goa.php?'+ category +'&'+ facility +'&'+ min_ticket_price +'&'+ max_ticket_price+'&'+min_distance+'&'+max_distance+'&'+min_age+'&'+max_age)
+                 
+                // saat response didapat               
                 .then(response => {
                     
                     this.results = response.data.data
                     this.switchPage("result-page")
                 })
+
+                // saat error didapat
                 .catch(errors => {
 
                     console.log(errors)
@@ -137,14 +226,24 @@ new Vue({
         },
         getDetail(id){
             
+            // alihkan ke halaman loading
             this.switchPage("loading-page")
+
+            // menggunakan library axio
+            // untuk melakukan http request
             axios
+
+                // url target
                 .get(baseURL + 'api/one_data_pariwisata.php?id=' + id)
+                
+                // saat response didapat
                 .then(response => {
                     
                     this.detail = response.data.data
                     this.switchPage("detail-page")
                 })
+
+                // saat error didapat
                 .catch(errors => {
 
                     console.log(errors)
@@ -152,40 +251,31 @@ new Vue({
                 }) 
 
         },
+
+        // fungsi untuk kembali ke halaman
+        // utama dan mereset data parameter request
         toHome(){
             this.param = {
+
+                // id kategori yg dipilih
                 category_choosed : 0,
-                facility : {
-                    label : "Fasilitas Wisata",
-                    value : "",
-                },
-                distance : {
-                    label : "Jarak",
-                    min_value : "",
-                    max_value : ""
-                },
-                ticket_price : {
-                    label : "Tiket Masuk",
-                    min_value : "",
-                    max_value : ""
-                },
-                age : {
-                    label : "Pilih Umur",
-                    min_value : "",
-                    max_value : ""
-                }
+
+                // data request untuk fasilias
+                facility : { label : "Fasilitas Wisata", value : "" },
+
+                // data request untuk jarak
+                distance : { label : "Jarak", min_value : "", max_value : "" },
+
+                // data request untuk harga tiket
+                ticket_price : { label : "Tiket Masuk", min_value : "", max_value : "" },
+
+                // data request untuk umur
+                age : { label : "Pilih Umur", min_value : "", max_value : "" }
             }
+
+            // alihkan ke hamalan kategori
             this.switchPage("category-page")
-        },
-        onBackPress(){
-            
-            if (this.page.name == "detail-page"){
-                this.switchPage("result-page")
-            } else if (this.page.name == "result-page"){
-                this.switchPage("criteria-page")
-            } else if (this.page.name == "criteria-page"){
-                this.switchPage("category-page")
-            }
-        },
+        }
+
     }
 })
